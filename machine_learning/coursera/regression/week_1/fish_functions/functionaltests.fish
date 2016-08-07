5e7cdf004183cee6991df6b710092008cfe3efbb
russellnakamura <r.nakamura.us@ieee.org>
2016-08-07 16:24:17 -0700

    Moved the coursera folder to its own repository

--- machine_learning/coursera/regression/week_1/fish_functions/functionaltests.fish
+++
@@ -1,5 +0,0 @@
-function functionaltests
-    jupyter nbconvert linear_regression_with_sklearn.ipynb --to python
-    jupyter nbconvert functional_tests.ipynb --to python
-    python functional_tests.py
-end
